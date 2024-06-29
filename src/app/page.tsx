7ee40c58c47cfcc8d8ba3841692919bf4fa6732b
Chris Thapa <117969522+pradip-leanq@users.noreply.github.com>
2024-06-30 00:03:01 +0545

    zz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link href="/memory-game">
-        <button className="bg-slate-700 rounded-md px-4 py-3 hover:scale-105 transition-all">
+        <Button>
           MemoryGame
-        </button>
+        </Button>
       </Link>
     </main>
   );
---
+++ src/app/rock-paper-scissors/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RockPaperScissorsPage() {
+  return (
+    <div>RockPaperScissorsPage</div>
+  )
+}
