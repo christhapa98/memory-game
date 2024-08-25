1ba4a587b5864280dd0a61c6c57154e5631d6e51
Chris Thapa <141107824+LeanQChris@users.noreply.github.com>
2024-08-25 23:07:21 +0545

    added bg

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default function MemoryGameLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className='bg-gradient-to-r from-amber-200 to-yellow-500 font-mono h-screen'>
+        <div style={{
+            backgroundImage: 'url(bg.jpg)',
+        }} className='bg-gradient-to-r from-amber-200 to-yellow-500 font-mono h-screen'>
             {children}
         </ div>
     )
